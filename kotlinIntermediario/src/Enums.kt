0bd60c88b54880a895f9435b7b727ffb024f5aa0
isaac <isaac.npcad@gmail.com>
2021-01-10 17:50:07 -0300

    Trabalhando com enums
    Semelhante ao trabalhar com Enum em Java

---
+++ kotlinIntermediario/src/Enums.kt
@@ -0,0 +1,16 @@
+
+enum class Direcao {
+    NORTE, SUL, LESTE, OESTE
+}
+
+class Bussola(var direcao: Direcao = Direcao.LESTE)
+
+fun main(args: Array<String>){
+    val bussola = Bussola()
+
+    println(bussola.direcao)
+
+    bussola.direcao = Direcao.NORTE
+
+    println(bussola.direcao)
+}
